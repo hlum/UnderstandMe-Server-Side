66c9629fe2da38dfcb6894b5f4dc7dacef3194d6
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-09-29 10:40:48 +0900

    Response::sendをデータも返せるようにする

--- src/Response.php
+++ src/Response.php
@@ -1,11 +1,11 @@
 <?php
 
 class Response {
-    public static function send(string $status, string $message, int $code = 200){
+    public static function send(string $status, string $message, int $code = 200, ?string $data = null) {
         http_response_code($code);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode(
-            ['status' => $status, 'message' => $message],
+            ['status' => $status, 'message' => $message, 'data' => $data],
             JSON_UNESCAPED_UNICODE
         );
         exit();
