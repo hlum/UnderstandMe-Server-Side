9aa0d0454cbf3c32718aa183dc832c8e8958c333
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-09-29 11:26:10 +0900

    HomeworkRepositoryInterfaceを追加

---
+++ src/Domain/Repositories/HomeworkRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domain\Repositories;
+
+use Domain\Entities\Homework;
+
+interface HomeworkRepositoryInterface {
+    public function insert(Homework $homework): void;
+    public function findById(string $id): ?Homework;
+    public function findByTeacherId(string $teacherId): array;
+    public function findByMajorId(string $majorId): array;
+    public function deleteById(string $id): void;
+}
