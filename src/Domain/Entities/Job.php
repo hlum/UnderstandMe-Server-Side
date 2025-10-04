011997b5c120e5d0ac5eb889457417480de2727f
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-04 16:42:39 +0900

    Job , Status を追加

---
+++ src/Domain/Entities/Job.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Domain\Entities;
+use JsonSerializable;
+use DateTimeImmutable;
+
+
+
+
+class Job implements JsonSerializable{
+    public string $id;
+    public string $projectId;
+    public Status $status;
+    public ?DateTimeImmutable $createdAt;
+    public DateTimeImmutable $updatedAt;
+
+    private function __construct(
+        string $id,
+        string $projectId,
+        Status $status,
+        ?DateTimeImmutable $createdAt,
+        DateTimeImmutable $updatedAt
+        ) {
+        $this->id = $id;
+        $this->projectId = $projectId;
+        $this->status = $status;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    public static function createNew(
+        string $projectId,
+        Status $status
+    ): self {
+        return new self(
+            bin2hex(random_bytes(16)),
+            $projectId,
+            $status,
+            new DateTimeImmutable(),
+            new DateTimeImmutable()
+        );
+    }
+
+    public static function fromDBRow(array $row): self {
+        return new self(
+            $row['id'],
+            $row['project_id'],
+            Status::from($row['status']),
+            isset($row['created_at']) ? new DateTimeImmutable($row['created_at']) : null,
+            new DateTimeImmutable($row['updated_at'])
+        );
+    }
+
+    public function jsonSerialize(): array {
+        return [
+            'id' => $this->id,
+            'project_id' => $this->projectId,
+            'status' => $this->status instanceof Status ? $this->status->getValue() : $this->status,
+            'created_at' => $this->createdAt ? $this->createdAt->format('Y-m-d H:i:s') : null,
+            'updated_at' => $this->updatedAt->format('Y-m-d H:i:s'),
+        ];
+    }
+}
---
+++ src/Domain/Entities/Status.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Domain\Entities;
+
+class Status {
+    private string $value;
+
+    public function __construct(string $value) {
+        $this->value = $value;
+    }
+
+    public function getValue(): string {
+        return $this->value;
+    }
+
+    public static function from(string $value): self {
+        if ($value === 'pending') {
+            return new self('pending');
+        } elseif ($value === 'processing') {
+            return new self('processing');
+        } elseif ($value === 'done') {
+            return new self('done');
+        } elseif ($value === 'failed') {
+            return new self('failed');  
+        } else {
+            throw new \InvalidArgumentException("Invalid status value: $value");
+        }
+    }
+}
