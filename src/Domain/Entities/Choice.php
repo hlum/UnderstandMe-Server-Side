f94cb9074816a1060980ce1b1c6b46dc0dda32bb
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-06 09:53:53 +0900

    Choice Entityを追加

---
+++ src/Domain/Entities/Choice.php
@@ -0,0 +1,58 @@
+<?php
+
+
+// Question 1個には５個の選択肢がある
+namespace Domain\Entities;
+use DateTimeImmutable;
+use JsonSerializable;
+
+class Choice implements JsonSerializable {
+    public string $id;
+    public string $questionId;
+    public string $choiceText;
+    public bool $isCorrect;
+
+
+    private function __construct(
+        string $id,
+        string $questionId,
+        string $choiceText,
+        bool $isCorrect
+    ) {
+        $this->id = $id;
+        $this->questionId = $questionId;
+        $this->choiceText = $choiceText;
+        $this->isCorrect = $isCorrect;
+    }
+
+    public static function createNew(
+        string $questionId,
+        string $choiceText,
+        bool $isCorrect
+    ): self {
+        return new self(
+            id: uniqid('cho_', true),
+            questionId: $questionId,
+            choiceText: $choiceText,
+            isCorrect: $isCorrect
+        );
+    }
+
+    public static function fromDBRow(array $row): self {
+        return new self(
+            id: $row['id'],
+            questionId: $row['question_id'],
+            choiceText: $row['choice_text'],
+            isCorrect: (bool)$row['is_correct']
+        );
+    }
+
+    public function jsonSerialize(): array {
+        return [
+            'id' => $this->id,
+            'question_id' => $this->questionId,
+            'choice_text' => $this->choiceText,
+            'is_correct' => $this->isCorrect,
+        ];
+    }
+}
