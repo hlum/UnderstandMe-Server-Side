7d3a16ad0b51ed3b93a1c478243cb58f967b34e0
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-19 18:05:24 +0900

    feat: 結果ユースケースを追加

---
+++ src/Application/UseCases/ResultUseCase.php
@@ -0,0 +1,39 @@
+<?php
+namespace Application\UseCases;
+
+use Domain\Repositories\ResultRepositoryInterface;
+use Domain\Entities\Result;
+
+class ResultUseCase
+{
+    private ResultRepositoryInterface $resultRepository;
+
+    public function __construct(ResultRepositoryInterface $resultRepository)
+    {
+        $this->resultRepository = $resultRepository;
+    }
+
+
+    public function getResult(string $resultID): Result
+    {
+        return $this->getResult($resultID);
+    }
+
+
+    public function fetchResultWithHomeworkIDAndUserID(string $homeworkID, string $userID): ?Result
+    {
+        return $this->resultRepository->fetchResultWithHomeworkIDAndUserID($homeworkID, $userID);
+    }
+
+
+    public function saveNewResult(Result $result)
+    {
+        $this->resultRepository->insertResult($result);
+    }
+
+
+    public function updateResult(string $resultID, int $score, int $correctAnswers): void
+    {
+        $this->resultRepository->updateResult($resultID, $score, $correctAnswers);
+    }
+}
