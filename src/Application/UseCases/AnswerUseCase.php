3d3381f5456f0ce5a0f7e487bed2f859bcbb263a
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-10-19 18:04:59 +0900

    feat: 回答ユースケースを追加

---
+++ src/Application/UseCases/AnswerUseCase.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Application\UseCases;
+
+use Domain\Repositories\AnswerRepositoryInterface;
+use Domain\Repositories\QuestionRepositoryInterface;
+use Domain\Repositories\ResultRepositoryInterface;
+use Domain\Entities\Answer;
+use Domain\Repositories\UserRepositoryInterface;
+
+
+class AnswerUseCase
+{
+    private AnswerRepositoryInterface $answerRepo;
+    private UserRepositoryInterface $userRepo;
+    private QuestionRepositoryInterface $questionRepo;
+
+
+    public function __construct(
+        AnswerRepositoryInterface $answerRepo,
+        UserRepositoryInterface $userRepo,
+        QuestionRepositoryInterface $questionRepo
+    ) {
+        $this->answerRepo = $answerRepo;
+        $this->userRepo = $userRepo;
+        $this->questionRepo = $questionRepo;
+    }
+
+    public function addAnswer(Answer $answer)
+    {
+        $this->validateAnswer($answer);
+        $this->answerRepo->addAnswer($answer);
+    }
+
+    /**
+     * 
+     * @param string $questionID
+     * @param string $userID
+     * @return Answer[]
+     */
+    public function findAnswer(string $questionID, string $userID): array
+    {
+        return $this->answerRepo->getAnswers($questionID, $userID);
+    }
+
+
+
+    private function validateAnswer(Answer $answer)
+    {
+        $user = $this->userRepo->findById($answer->userID);
+        if ($user === null) {
+            throw new \InvalidArgumentException('指定されたuser_idが存在しません。');
+        }
+
+        $question = $this->questionRepo->findById($answer->questionID);
+        if ($question === null) {
+            throw new \InvalidArgumentException('指定されたquestion_idが存在しません。');
+        }
+    }
+}
