a70f73b306cf0960748a288eb7cd75bc1bd2c6de
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-11-11 15:21:08 +0900

    revert: MySQLResultRepository

--- src/Domain/Repositories/ResultRepositoryInterface.php
+++ src/Domain/Repositories/ResultRepositoryInterface.php
@@ -7,5 +7,10 @@ interface ResultRepositoryInterface
 {
     public function insertResult(Result $result);
     public function fetchResultWithHomeworkIDAndUserID(string $homeworkID, string $userID): ?Result;
+
+    /**
+     * @return Result[]
+     */
+    public function fetchResultsByUserID(string $userID, int $year): array;
     public function updateResult(string $resultID, int $score, int $correctAnswers);
 }
