a2d8c699a01a21f92d78026582fbd715150d3dc3
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-12-02 10:18:12 +0900

    feat: update_correect_choiceをtransaction使ってデータの一貫性を確保する

--- public/choices/update_correct_choice.php
+++ public/choices/update_correct_choice.php
@@ -43,13 +43,30 @@ try {
     }
 
     $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    // データの一貫性を保つためにトランザクションを開始
+    $connection->begin_transaction();
+
     $choiceRepository = new MySQLChoiceRepository($connection);
     $questionRepository = new MySQLQuestionRepository($connection);
     $choiceUseCase = new ChoiceUseCase($choiceRepository, $questionRepository);
     $choiceUseCase->updateCorrectChoice($newCorrectChoiceID, $questionID);
+
+    // すべて成功した場合、トランザクションをコミット
+    $connection->commit();
     Response::send('success', '正しい選択肢が正常に更新されました。', 200);
 } catch (ValidationException $ve) {
+    if (isset($connection)){
+        $connection->rollback();
+    }
     Response::send('fail', $ve->getMessage(), 400, null, 'validation_error');
 } catch (Exception $e) {
+    if (isset($connection)){
+        $connection->rollback();
+    }
     Response::send('error', 'サーバーエラーが発生しました。', 500, null, 'server_error');
+} finally {
+    if (isset($connection)){
+        $connection->close();
+    }
 }
